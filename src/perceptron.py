8c90b86fa6b3238bfa67d7bcd2a7f21dfde624a9
Tomasz Cichecki <knight@kopernet.org>
2009-10-24 13:46:46 +0200

    refactoryzacja - bardziej przejrzyste nazwy i układ

--- src/perceptron.py
+++ src/perceptron.py
@@ -3,11 +3,11 @@ class Neuron(object):
     '''
     classdocs
     '''
-    def __init__(self, init_vector=[0,1,1]):
+    def __init__(self, initial_weights=[0,1,1]):
         '''
         Constructor
         '''
-        self.weights = init_vector
+        self.weights = initial_weights
         self.learn_factor = 0.2
     def train(self, epoch, max_iter=100):
         i = 0
@@ -20,11 +20,17 @@ class Neuron(object):
             self.learn(epoch[i])
     def learn(self, vector):
         inputs = vector[0]        
-        answer = vector[1]
+        requested = vector[1]
 
-        s = self.answer(inputs)
-        if s != answer:
-            self.correct_weights(answer-s,inputs)
+        current_answer = self.answer(inputs)
+        if current_answer != requested:
+            self.correct_weights(requested-current_answer,inputs)
+    def correct_weights(self,d,inputs):
+        self.weights[0] = self.correct_weight(self.weights[0], d, -1)
+        for i in range(1, len(self.weights)):
+            self.weights[i] = self.correct_weight(self.weights[i], d, inputs[i-1])
+    def correct_weight(self, weight, delta, x):
+        return (weight + self.learn_factor * delta * x)            
     def is_epoch_learnt(self, epoch):
         for i in range(0, len(epoch)):
             expected_answer, data_vector = epoch[i][1], epoch[i][0]
@@ -32,8 +38,8 @@ class Neuron(object):
                 return False
         return True
     def answer(self, data_vector):
-        w0 = -1.0 
-        s = self.weights[0] * w0
+        x0 = -1.0 
+        s = self.weights[0] * x0
         for i in range(1, len(self.weights)):
             s += self.weights[i] * data_vector[i-1]
         return self.activation(s)
@@ -43,9 +49,4 @@ class Neuron(object):
             return -1
         else:
             return 1
-    def correct_weights(self,d,inputs):
-        self.weights[0] = self.correct_weight(self.weights[0], d, -1)
-        for i in range(1, len(self.weights)):
-            self.weights[i] = self.correct_weight(self.weights[i], d, inputs[i-1])
-    def correct_weight(self, weight, delta, x):
-        return (weight + self.learn_factor * delta * x)
+
